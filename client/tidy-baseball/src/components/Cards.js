ba33010c7fcd364d572f5e9aa304fec7c9d19e78
Forace Avant <themattavant@gmail.com>
2020-11-04 21:10:05 -0800

    created footer for all pages and began insert pictures for cards

--- client/tidy-baseball/src/components/Cards.js
+++ client/tidy-baseball/src/components/Cards.js
@@ -12,8 +12,34 @@ function Cards() {
                     <ul className="cards__items">
                         <CardItem 
                         src='images/img-9.jpg'
+                        text="Here are some of our lower-budget highschool fields!"
+                        label='Highschool Field Services'
+                        path='/services'
+                        />
+                        <CardItem 
+                        src='images/img-2.jpg'
+                        text="Here are some of our college/professional fields!"
+                        label='Professional Field Services'
+                        path='/services'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                        src='images/img-3.jpg'
                         text="Here is some default text that I need to change in cards"
                         label='Baseball'
+                        path='/services'
+                        />
+                        <CardItem 
+                        src='images/img-4.jpg'
+                        text="Here is some default text that I need to change in cards"
+                        label='Baseball numba 2'
+                        path='/services'
+                        />
+                        <CardItem 
+                        src='images/img-5.jpg'
+                        text="Here is some default text that I need to change in cards"
+                        label='Baseball numba 2'
                         path='/services'
                         />
                     </ul>
